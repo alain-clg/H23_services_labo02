a05bb24b08225e6374bc6d68eb391f0c487e49f4
Alain Pilon <Alain.Pilon@clg.qc.ca>
2023-03-22 15:26:12 -0400

    Modification de la librairie mongoose vers la version 7.0.2 avec promesses

--- routes/apiLivres.js
+++ routes/apiLivres.js
@@ -1,61 +1,57 @@
+// Fichier routes des /api/livres
+// Auteur: Alain Pilon
+// Date: 22 mars 2023
 const express = require('express');
 const router = express.Router();
 
 const Livres = require('../modeles/Livres.js');
+const mongoose = require('mongoose');
 
 router.get('/', (requete, reponse) => {
-    Livres.getLivres((err, livres)=>{
-        if (err) throw err;
-        reponse.json(livres);
-    }, 25);
-});
-
-router.post('/', (requete, reponse) => {
-    let livre = requete.body;
-    // console.log(livre);
-    Livres.ajoutLivre( livre, (err, retourLivre)=>{
-        if (err) throw err;
-        reponse.json(retourLivre);
-    });
-});
-
-router.put('/:isbnLivre', (requete, reponse)=>{
-    let nouveauLivre = requete.body;
-    Livres.modifierLivre(requete.params.isbnLivre, nouveauLivre, (err, resultat) => {
-        if (err) throw err;
-        reponse.json(resultat);
-    });
-});
-router.delete('/:isbnLivre', (requete, reponse)=>{
-    Livres.supprimerLivre(requete.params.isbnLivre, (err, livre) => {
-        if (err) throw err;
-        reponse.json(livre);
-    });
+    Livres.find({}, null, {limit: 25, sort: {auteur: 1}}).exec()
+    .then((livres)=>reponse.json(livres))
+    .catch((err)=>console.log(err));
 });
 router.get('/titre/:titreLivre', (requete, reponse) => {
-    Livres.getLivreParChamp("titre", requete.params.titreLivre, (err, livre)=>{
-        if (err) throw err;
-        reponse.json(livre);
-    });
+    Livres.find({titre: RegExp(requete.params.titreLivre, "i")}, null, {limit: 25, sort: {auteur: 1}}).exec()
+    .then((livres)=>reponse.json(livres))
+    .catch((err)=>console.log(err));
 });
 router.get('/auteur/:auteurLivre', (requete, reponse) => {
-    Livres.getLivreParChamp("auteur", requete.params.auteurLivre, (err, livre)=>{
-        if (err) throw err;
-        reponse.json(livre);
-    });
+    Livres.find({auteur: RegExp(requete.params.auteurLivre, "i")}, null, {limit: 25, sort: {auteur: 1}}).exec()
+    .then((livres)=>reponse.json(livres))
+    .catch((err)=>console.log(err));
 });
 router.get('/isbn/:isbnLivre', (requete, reponse) => {
-    Livres.getLivreParChamp("isbn", requete.params.isbnLivre, (err, livre)=>{
-        if (err) throw err;
-        reponse.json(livre);
-    });
+    Livres.find({isbn: RegExp(requete.params.isbnLivre, "i")}, null, {limit: 25, sort: {auteur: 1}}).exec()
+    .then((livres)=>reponse.json(livres))
+    .catch((err)=>console.log(err));
 });
 router.get('/description/:descLivre', (requete, reponse) => {
-    Livres.getLivreParChamp("description", requete.params.descLivre, (err, livre)=>{
-        if (err) throw err;
-        reponse.json(livre);
-    });
+    Livres.find({description: RegExp(requete.params.descLivre, "i")}, null, {limit: 25, sort: {auteur: 1}}).exec()
+    .then((livres)=>reponse.json(livres))
+    .catch((err)=>console.log(err));
 });
-
+router.post('/', (requete, reponse) => {
+    let livre = requete.body;
+    livre._id = new mongoose.Types.ObjectId();
+    Livres.create(livre)
+    .then(resultat=>reponse.json(resultat))
+    .catch(err=>console.log(err));
+});
+router.put('/:isbnLivre', (requete, reponse)=>{
+    let nouveauLivre = requete.body;
+    Livres.findOneAndUpdate(
+        {isbn: requete.params.isbnLivre},
+        nouveauLivre, 
+        { new: true })
+    .then(resultat=>reponse.json(resultat))
+    .catch(err=>console.log(err));
+});
+router.delete('/:isbnLivre', (requete, reponse)=>{
+    Livres.deleteOne({isbn: requete.params.isbnLivre})
+    .then(res=>reponse.json(res))
+    .catch(err=>console.log(err));
+});
 
 module.exports = router;
